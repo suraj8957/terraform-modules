46a7daece94bf5a148d4e21b5a9b5154dad0c6c3
suraj8957 <suraj.tripathi.snaatak@mygurukulam.co>
2026-05-29 04:11:10 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -11,7 +11,7 @@ provider "aws" {
    region = var.region
 }
 resource "aws_instance" "instance" {
-  ami           = var.instance_ami
+  ami           = var.ami
   instance_type = "t2.micro"
 
   tags = {
