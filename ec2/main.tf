246c80b4ff58b39d148f61377beab4c995ef692c
suraj8957 <suraj.tripathi.snaatak@mygurukulam.co>
2026-05-29 01:55:34 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.46.0"
+    }
+  }
+}
+
+provider "aws" {
+   region = var.region
+}
 resource "aws_instance" "instance" {
   ami           = var.instance_ami
   instance_type = "t2.micro"
