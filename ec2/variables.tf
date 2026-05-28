e31deed717f21f4f1159eb5c0abe330ca92fd461
suraj8957 <suraj.tripathi.snaatak@mygurukulam.co>
2026-05-29 04:05:19 +0530

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,16 +1,17 @@
 variable "region" {
   type = string
-  default = "us-east-1"
   description = "provide region where ec2 creates"
 }
-variable "instance_ami" {
+variable "ami" {
   type        = string
-  default     = "ami-091138d0f0d41ff90"
   description = "instance ami for dev/qa"
+}
+variable "instance_type" {
+  type        = string
+  description = "Instance Name for dev/qa"
 }
 
 variable "instance_name" {
   type        = string
-  default     = "d-instance"
   description = "Instance Name for dev/qa"
 }
